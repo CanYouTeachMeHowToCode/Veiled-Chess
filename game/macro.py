af12c16ee428467e7a77130ad0d7223d283e1c7b
CanYouTeachMeHowToCode <yilunw1030@gmail.com>
2023-12-19 03:35:19 -0500

    change board theme to chess.com theme, looks much better

--- game/macro.py
+++ game/macro.py
@@ -3,8 +3,8 @@
 # Chess game macros
 UNICODE_PIECE_SYMBOLS = u'.●♟♞♝♜♛♚♔♕♖♗♘♙○'
 ASCII_PIECE_CHARS = '.VPNBRQKkqrbnpv'
-COLOR_WHITE = (220, 208, 194)
-COLOR_BLACK = (53, 53, 53)
+COLOR_WHITE = (234, 235, 200) 
+COLOR_BLACK = (119, 154, 88)
 COLOR_GREEN = (0, 255, 100)
 PLAYER_WHITE = 'white'
 PLAYER_BLACK = 'black'
